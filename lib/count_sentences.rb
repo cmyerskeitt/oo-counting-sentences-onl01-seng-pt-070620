dc09382eaa53243db4a59c0f270537415c462565
Christine Myers Keitt <cmyerskeitt@gmail.com>
2020-07-23 19:04:48 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -27,7 +27,7 @@ class String
   end
 
   def count_sentences
-    new_array= self.split("/.?!/").reject {|string| string.empty?}.length 
+    new_array= self.split("/.?!/").reject {|string| string.empty?}
   binding.pry
   end
 end
